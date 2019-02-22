13012883363f9943d712908298958930b877597b
Tebiba <Tebiba@users.noreply.github.com>
2019-02-22 20:19:21 +0300

    Modified the If statements, variables and display of the statement

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 
 $(document).ready(function() {
-    $("input[type='button']").click(function() {
+    $("input[type='button']").click(function(event) {
         var question1 = parseInt($('input[name = "quest1"]:checked').val());
         var question2 = parseInt($('input[name = "quest2"]:checked').val());
         var question3 = parseInt($('input[name = "quest3"]:checked').val());
@@ -14,7 +14,7 @@ $(document).ready(function() {
 
       var result = question1 + question2 + question3 + question4 + question5 + question6 + question7 + question8 + question9 + question10;
 
-      document.getElementById("output").innerHTML = "You have scored" + result;
+      document.getElementById("output").innerHTML = result;
     });
 
   });
