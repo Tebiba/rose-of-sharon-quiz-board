ffdac82b81d0aff08994c41deb1f7cda6d42b67f
Tebiba <Tebiba@users.noreply.github.com>
2019-02-23 16:55:22 +0300

    Adding additional button for comments

--- js/scripts.js
+++ js/scripts.js
@@ -15,6 +15,9 @@ $(document).ready(function() {
       var result = question1 + question2 + question3 + question4 + question5 + question6 + question7 + question8 + question9 + question10;
 
       document.getElementById("output").innerHTML = result;
+
+
+      event.preventDefault();
     });
 
   });
